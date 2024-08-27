5a5cacb0e0f58aa827a926110917400d0b241625
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2024-08-27 22:10:30 +0530

    Time: 2 ms (67.07%) | Memory: 14.6 MB (78.43%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        //bruteforce
+        //next_permutation(nums.begin(), nums.end());
+
+        //optimal
+        int ind = -1;
+        for(int i = nums.size()-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                ind = i;
+                break;
+            }
+        }
+         if (ind == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        for(int i = nums.size()-1; i>ind; i--){
+            if(nums[i]>nums[ind]) {
+                swap(nums[i], nums[ind]);
+                break;
+            }
+        }
+
+        reverse(nums.begin()+ind+1, nums.end());
     }
 };
