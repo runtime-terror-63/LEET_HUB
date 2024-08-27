05c3a661f91f4d66a5ae0fae589b58142fd836c5
runtime-terror-63 <94905513+runtime-terror-63@users.noreply.github.com>
2024-08-27 19:05:14 +0530

    Time: 84 ms (63.17%) | Memory: 100.2 MB (5.00%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+       /* int buy = min(prices.begin(), prices.end());
+        int sell = max(prices[buy]+1, prices.end());
+        if(buy<sell)  */
+
+       int n = prices.size();
+        vector<int> maxPrices(n, 0);
+        maxPrices[n-1] = prices[n-1];
+        for(int i=n-2; i>=0; i--){
+            maxPrices[i] = max(maxPrices[i+1], prices[i]); 
+        }
+        int maxProfit = 0;
+        for(int i=0; i<n; i++){
+            maxProfit = max(maxProfit, maxPrices[i] - prices[i]); 
+        }
+        return maxProfit;
+    }
+};
