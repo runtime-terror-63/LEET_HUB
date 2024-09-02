0e4743cd96acb47a16d10222e8dc559720802870
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2024-09-02 09:22:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (7.51%) - LeetSync

--- 290-word-pattern/word-pattern.cpp
+++ 290-word-pattern/word-pattern.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        vector<string> words;
         string word;
+        vector<string>words;
         stringstream ss(s);
-        while (ss >> word) {
-            words.push_back(word);
-        }
-        
-        if (pattern.length() != words.size()) return false;
-        
-        unordered_map<char, string> mp1;
-        unordered_map<string, char> mp2;
-        
-        for (int i = 0; i < pattern.length(); i++) {
+        unordered_map<char, string>mp1;
+        unordered_map<string, char>mp2;
+        while(ss>>word) words.push_back(word);
+
+        if(pattern.length() != words.size()) return false;
+        for(int i = 0; i<pattern.length(); i++){
             char c = pattern[i];
             string w = words[i];
-            
-            if (mp1.find(c) != mp1.end() && mp1[c] != w) 
-                return false;
-            if (mp2.find(w) != mp2.end() && mp2[w] != c) 
-                return false;
-            
+
+            if(mp1.find(c) != mp1.end()){
+                if(mp1[c] != w) return false;
+            }
+
+            if(mp2.find(w) != mp2.end()){
+                if(mp2[w] != c) return false;
+            }
+
             mp1[c] = w;
             mp2[w] = c;
+
         }
         return true;
     }
