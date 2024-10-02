5b05d7dc5d658f4a9632bedccb348077f5df08d8
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2024-10-02 13:18:26 +0530

    Time: 22 ms (26.33%) | Memory: 16.3 MB (77.51%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -17,12 +17,25 @@ public:
     }
     return true;
    }
+
+bool dfs(int s, int i, vector<int>& color, vector<vector<int>>& graph) {
+    color[s] = i;
+    for (auto it : graph[s]) {
+        if (color[it] == -1) {
+            if (dfs(it, !i, color, graph) == false) return false;
+        } 
+        else if (color[it] == color[s]) {
+            return false;  
+        }
+    }
+    return true;
+}
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int>color(n, -1);
         for(int i = 0; i<n; i++){
             if(color[i]==-1){
-                if(bfs(i, color, graph)==false) return false;
+                if(dfs(i, 0, color, graph)==false) return false;
             }
         }
         return true;
