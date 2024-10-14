1183a9e15a08021a378f6e2a44d66314bbc3b104
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2024-10-14 16:55:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (51.84%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,50 +1,21 @@
-#include <iostream>
-using namespace std;
-
 class Solution {
 public:
-    int rat(int** a, int m, int n, int i, int j, int** dp) {
-        if (i == m - 1 && j == n - 1) {
-            return 1;
-        }
-        if (i >= m || j >= n || a[i][j] == -1) {
-            return 0;
-        }
-        // Check if the result is already computed
-        if (dp[i][j] != -5) {
-            return dp[i][j];
-        }
-       
-        int right = rat(a, m, n, i, j + 1, dp); 
-        int down = rat(a, m, n, i + 1, j, dp);   
+    int solve(int m, int n, vector<vector<int>>&dp){ 
 
-        // Store the result in the dp array
-        dp[i][j] = right + down;
-        return right + down;
+        if(m==0 && n==0){
+       return 1;
+        }
+
+    if(m < 0 || n < 0){
+        return 0;
     }
-
+        if(dp[m][n]!=-1)return dp[m][n];
+        int up = solve(m-1, n, dp);
+        int left = solve(m, n-1, dp);
+        return dp[m][n] = up+left;
+    }
     int uniquePaths(int m, int n) {
-        // Allocate memory for 2D arrays
-        int** grid = new int*[m];
-        int** dp = new int*[m];
-        for (int i = 0; i < m; ++i) {
-            grid[i] = new int[n](); // Initialize with 0
-            dp[i] = new int[n];
-            fill(dp[i], dp[i] + n, -5); // Initialize with -5
-        }
-
-        // Compute the number of unique paths
-        int result = rat(grid, m, n, 0, 0, dp);
-
-        // Deallocate memory
-        for (int i = 0; i < m; ++i) {
-            delete[] grid[i];
-            delete[] dp[i];
-        }
-        delete[] grid;
-        delete[] dp;
-
-        return result;
+        vector<vector<int>>dp(m, vector<int>(n, -1));
+        return solve(m-1, n-1, dp);
     }
 };
-
