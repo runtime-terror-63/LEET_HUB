9e714255032e0ecd1e00a8f23ee6f108c764b31c
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2024-10-25 13:51:57 +0530

    Time: 7 ms (98.15%) | Memory: 21.8 MB (43.05%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    long long  func(vector<int> &arr, int n, int mid, int h)
+{
+  long long  total = 0;
+  for (int i = 0; i < n; i++)
+  {
+    total += ceil((double)(arr[i]) / (double)(mid));
+  }
+  return total;
+}
+int findAns(vector<int> &arr, int n, int h)
+{
+  int low = 1;
+  int high = *max_element(arr.begin(), arr.end());
+  int ans = INT_MAX;
+  while (low <= high)
+  {
+    int mid = (low + high) >> 1;
+    if (func(arr, n, mid, h) <= h)
+    {
+      ans = min(ans, mid);
+      high = mid - 1;
+    }
+    else
+      low = mid + 1;
+  }
+  return ans;
+}
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        return findAns(piles, n, h);
+    }
+};
