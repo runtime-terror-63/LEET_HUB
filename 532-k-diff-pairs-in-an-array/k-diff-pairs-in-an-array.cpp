8f375f8a07edc19388e6ef03eb2e323d15c72d1c
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2024-10-26 22:44:34 +0530

    Time: 12 ms (66.27%) | Memory: 16.2 MB (43.92%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    int bs(vector<int> &nums, int low, int j)
+int bs(vector<int> &arr, int target, int low, int n)
 {
-  int high = nums.size() - 1;
+  int high = arr.size() - 1;
   while (low <= high)
   {
     int mid = (low + high) >> 1;
-    if (nums[mid] == j)
+    if (arr[mid] == target)
       return mid;
-    else if (nums[mid] < j)
+    else if (arr[mid] < target)
       low = mid + 1;
     else
       high = mid - 1;
   }
   return -1;
 }
-
-int findPairs(vector<int> &nums, int k)
+int findPairs(vector<int> &arr, int k)
 {
-  sort(nums.begin(), nums.end());
-  int n = nums.size();
+    int n = arr.size();
+  sort(arr.begin(), arr.end());
   set<pair<int, int>> st;
   for (int i = 0; i < n; i++)
   {
-    if (bs(nums, i + 1, nums[i] + k) != -1)
-      st.insert({nums[i], nums[i] + k});
+    if (bs(arr, arr[i] + k, i + 1, n) != -1)
+      st.insert({arr[i] + k, arr[i]});
   }
   return st.size();
 }
+   
 };
