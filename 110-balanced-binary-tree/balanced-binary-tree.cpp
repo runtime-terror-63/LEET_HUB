5a42ac50fbc231718acbf53eeab8067ea4680f73
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2024-10-27 17:27:32 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (69.31%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,41 +11,15 @@
  */
 class Solution {
 public:
-/*
-    int height(TreeNode* root)
-    {
-        //either root is null return null
-        if(root==NULL) return NULL;
-        //height of lh...
-        int lh = height(root->left);
-        //height of rh...
-        int rh = height(root->right);
-        //absolute value of l and rh.. is greter then 1 the return -1;
-        if(abs(lh-rh)>1) return -1;
-        //if lh is -1 the return -1..
-        if(lh == -1) return -1;
-        //id rh is -1 the return -1..
-        if(rh == -1) return -1;
-        //max of lh and rh ....
-        return max(lh, rh)+1;
-
-        return 1;
-
-    }
-    */
-
-    int height(TreeNode* root){
-        if(root==NULL) return 0;
-        int lh = height(root->left);
-        int rh = height(root->right);
-        if(abs(lh-rh)>1) return  -1;
-        if(lh==-1) return -1;
-        if(rh==-1) return -1;
-        return max(lh, rh)+1;
-    }
+   int height(TreeNode* root){
+    if(root==NULL) return 0;
+    int lh = height(root->left);
+    int rh = height(root->right);
+    if(abs(rh-lh)>1) return -1;
+    if(lh==-1 || rh==-1) return -1;
+    return 1 + max(lh,rh);
+   }
     bool isBalanced(TreeNode* root) {
-        return height(root) != -1;
-
-       
+       return height(root)!=-1;
     }
 };
