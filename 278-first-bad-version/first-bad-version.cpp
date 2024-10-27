0fb5f0cf1c5c5ef91ac9d5fc2de7d6412a7e4ddb
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2024-10-27 11:08:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (64.51%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -6,12 +6,14 @@ public:
     int firstBadVersion(int n) {
         if(n==1) return n;
         int low =1, high = n;
+        int ans = 0;
         while(low<=high){
             int mid = low + (high-low)/2;
             if(isBadVersion(mid)){
+                ans = mid;
                 high = mid-1;
             }else low = mid+1;
         }
-        return low;
+        return ans;
     }
 };
