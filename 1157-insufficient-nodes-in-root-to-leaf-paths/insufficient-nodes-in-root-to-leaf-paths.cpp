070687277fcb42872e204c315eea5cea7a6f1be9
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2024-11-04 14:04:23 +0530

    Time: 0 ms (100.00%) | Memory: 33.9 MB (83.08%) - LeetSync

---
+++ 1157-insufficient-nodes-in-root-to-leaf-paths/insufficient-nodes-in-root-to-leaf-paths.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sufficientSubset(TreeNode* root, int limit, int sum = 0) {
+        if(root==NULL) return nullptr;
+       
+        sum += root->val;
+        if(!root->left && !root->right){
+            if(sum<limit) return nullptr;
+            return root;
+        }
+        root->left =  sufficientSubset(root->left, limit, sum);
+        root->right =  sufficientSubset(root->right, limit, sum);
+
+        if(root->left==NULL && root->right ==NULL) return nullptr;
+        return root;
+    }
+};
