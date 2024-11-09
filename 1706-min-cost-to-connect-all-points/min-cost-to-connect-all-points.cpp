b2edd2fef34c3f14ddf3a8b942941f7f95e0eafd
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2024-11-09 12:06:21 +0530

    Time: 87 ms (83.69%) | Memory: 46.1 MB (76.27%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int n = points.size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<int> visited(n, 0);
-        int sum = 0;
-        int count = 0;
-        
-        pq.push({0, 0}); 
-        
-        while (!pq.empty() && count < n) {
-            auto [weight, node] = pq.top();
-            pq.pop(); 
-            if (visited[node]) continue;
-            visited[node] = 1;
-            sum += weight;
-            count++;
-            for (int next = 0; next < n; next++) {
-                if (!visited[next]) {
-                    int cost = abs(points[node][0] - points[next][0]) + abs(points[node][1] - points[next][1]);
-                    pq.push({cost, next});
-                }
-            }
-        }
-        return sum;
+          int n = points.size();
+  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+  pq.push({0, 0});
+  vector<int> vis(n, 0);
+  int sum = 0;
+  int cnt = 0;
+  while (!pq.empty() && cnt < n)
+  {
+    auto it = pq.top();
+    pq.pop();
+    int wt = it.first;
+    int node = it.second;
+    if (vis[node])
+      continue;
+    vis[node] = 1;
+    sum += wt;
+    cnt++;
+    for (int i = 0; i < n; i++)
+    {
+      if (!vis[i])
+      {
+        int cost = abs(points[node][0] - points[i][0]) + abs(points[node][1] - points[i][1]);
+        pq.push({cost, i});
+      }
+    }
+  }
+  return sum;
     }
 };
