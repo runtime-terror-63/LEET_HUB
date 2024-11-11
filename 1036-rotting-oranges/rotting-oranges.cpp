ea94fb84095c49af8ead74eb2d1b2c21e0c2c994
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2024-11-11 12:17:36 +0530

    Time: 3 ms (32.55%) | Memory: 16.6 MB (24.29%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -2,44 +2,43 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
-        int m = grid[0].size();
-        queue<pair<pair<int, int>, int>>q;
-        int frsh_cnt = 0;
-        vector<vector<int>>vis(n, vector<int>(m, 0));
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(grid[i][j]==2) {
-                    q.push({{i, j}, 0});
-                    vis[i][j] = 2;
-                }
-                else vis[i][j] = 0;
-                if(grid[i][j]==1)frsh_cnt++;
-            }
-        }
-        int delrow[] = {-1, 1, 0, 0};
-        int delcol[] = {0, 0, 1, -1};
-        int cnt = 0;
-        int tm = 0;
-        while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int time = q.front().second;
-            q.pop();
-            tm = max(tm, time);
-            for(int i = 0; i<4; i++){
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
-                    vis[nrow][ncol] = 2;
-                    q.push({{nrow, ncol}, time+1});
-                    cnt++;
-                }
-            }
-        }
-        if (cnt == frsh_cnt) {
-            return tm;
-        } else {
-            return -1;
-        }
+  int m = grid[0].size();
+  int frsh_cnt = 0;
+  queue<pair<pair<int, int>, int>>q;
+  vector<vector<int>>vis(n, vector<int>(m, 0));
+  for(int i=0; i<n; i++){
+    for(int j = 0; j<m; j++){
+      if(grid[i][j]==2){
+        q.push({{i, j}, 0});
+        vis[i][j] = 2;
+      }else vis[i][j] = 0;
+
+      if(grid[i][j]==1) frsh_cnt++;
+    }
+  }
+
+  int delRow[4] = {-1, 0, +1, 0};
+  int delCol[4] = {0, +1, 0, -1};
+  int cnt = 0;
+  int tm = 0;
+  while(!q.empty()){
+    int row = q.front().first.first;
+    int col = q.front().first.second;
+    int time = q.front().second;
+    q.pop();
+    tm = max(tm, time);
+    for(int i = 0; i<4; i++){
+      int nr = row + delRow[i];
+      int nc = col + delCol[i];
+
+      if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1 && !vis[nr][nc]){
+        vis[nr][nc] = 2;
+        q.push({{nr, nc}, time+1});
+        cnt++;
+      }
+    }
+  }
+        if(cnt==frsh_cnt) return tm;
+        else return -1;
     }
 };
