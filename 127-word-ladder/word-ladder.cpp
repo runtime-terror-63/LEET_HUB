2c3a4707bb1efe8bcdd564d32c3655fbc9c1df5f
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2024-11-12 10:42:32 +0530

    Time: 64 ms (66.32%) | Memory: 19.7 MB (69.96%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-            unordered_set<string> wordSet(begin(wordList), end(wordList));
-    if (!wordSet.count(endWord))
-      return 0;
+unordered_set<string> st(wordList.begin(), wordList.end());
+  queue<pair<string, int>> q;
+  q.push({beginWord, 1});
+  st.erase(beginWord);
 
-    int ans = 0;
-    queue<string> q{{beginWord}};
+  while (!q.empty())
+  {
+    string word = q.front().first;
+    int step = q.front().second;
+    q.pop();
+    for (int i = 0; i < word.size(); i++)
+    {
+      char orignal = word[i];
+      for (char ch = 'a'; ch <= 'z'; ch++)
+      {
+        word[i] = ch;
 
-    while (!q.empty()) {
-      ++ans;
-      for (int sz = q.size(); sz > 0; --sz) {
-        string word = q.front();
-        q.pop();
-        for (int i = 0; i < word.length(); ++i) {
-          const char cache = word[i];
-          for (char c = 'a'; c <= 'z'; ++c) {
-            word[i] = c;
-            if (word == endWord)
-              return ans + 1;
-            if (wordSet.count(word)) {
-              q.push(word);
-              wordSet.erase(word);
-            }
-          }
-          word[i] = cache;
+        if (st.find(word) != st.end())
+        {
+          st.erase(word);
+          q.push({word, step + 1});
         }
       }
+      word[i] = orignal;
     }
 
-    return 0;
+    if (word == endWord)
+      return step;
+  }
+  return 0;
     }
 };
