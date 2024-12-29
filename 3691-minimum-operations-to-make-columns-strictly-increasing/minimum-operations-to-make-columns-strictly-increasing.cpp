507f7f56dc6419c9063392a790201f001763af31
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2024-12-29 17:36:04 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (8.33%) - LeetSync

---
+++ 3691-minimum-operations-to-make-columns-strictly-increasing/minimum-operations-to-make-columns-strictly-increasing.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumOperations(vector<vector<int>>& grid) {
+    int n = grid.size();
+  int m = grid[0].size();
+  int cnt = 0;
+  for(int i=1; i<n; i++){
+    for(int j=0; j<m; j++){
+      if(grid[i][j] <= grid[i-1][j]){
+         int value = grid[i-1][j]- grid[i][j]+1;
+         grid[i][j] += value;
+         cnt+= value;
+      }
+    }
+  }
+  return cnt;
+    }
+};
