ab208ac757404f1a21d1f576fae444377b41b0ef
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2025-01-04 10:08:23 +0530

    Time: 47 ms (34.09%) | Memory: 49.3 MB (23.24%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     
-    int fn(int ind, vector<int>&arr, int target, vector<vector<int>>&dp){
-        //Base Case:
-        if(ind==0){
-            if(target%arr[0]==0) return target/arr[0];
-            else return 1e9;
-        }
-        if(dp[ind][target]!=-1) return dp[ind][target];
-        int non_take = 0 + fn(ind-1, arr, target, dp);
-        int take = 1e9;
-        if(target>=arr[ind]) take =  1 + fn(ind, arr, target-arr[ind], dp);
+int fn(int ind, vector<int>&coins, vector<vector<int>>&dp, int amount, int n){
+  if(ind==0){
+    if(amount%coins[0]==0) return amount/coins[0];
+    else return 1e9;
+  }
+  //take not_take
+  if(dp[ind][amount]!=-1) return dp[ind][amount];
 
-        return dp[ind][target] = min(take, non_take);
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>dp(coins.size(), vector<int>(amount+1, -1));
-        int ans = fn(coins.size()-1, coins, amount, dp);
-        if(ans>=1e9) return -1;
-        return ans;
-    }
+  int not_take = 0 + fn(ind-1, coins, dp, amount, n);
+  int take = 1e9;
+  if(amount>=coins[ind]) take = 1 + fn(ind, coins, dp, amount-coins[ind], n);
+
+  return dp[ind][amount] =  min(take, not_take);
+}
+int coinChange(vector<int>& coins, int amount) {
+  int n = coins.size();
+  vector<vector<int>>dp(n, vector<int>(amount+1, -1));
+  int ans =  fn(n-1, coins, dp, amount, n);
+  if(ans>=1e9) return -1;
+  return ans;
+}
 };
