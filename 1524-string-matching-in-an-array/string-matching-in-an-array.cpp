1fcab39ab7af09d6410002b13f65860745eae078
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2025-01-07 09:37:58 +0530

    Time: 6 ms (21.77%) | Memory: 11.5 MB (43.54%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -4,7 +4,9 @@ public:
         vector<string>res;
         int n = words.size();
         for(int i=0; i<n; i++){
+           
             for(int j=0; j<n; j++){
+               
                 if(i!=j && words[j].find(words[i]) != string::npos){
                   res.push_back(words[i]);
                   break;
