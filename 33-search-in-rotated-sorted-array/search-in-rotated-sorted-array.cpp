0b6313e7eb9e80c0e32810b53a3e5aec92d7efc2
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2025-04-06 09:24:20 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (33.80%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    int binarysearch(vector<int>&arr, int n, int target){
-        int low=0, high=n-1;
-        while(low<=high){
-            int mid = (low+high)>>1;
-            if(arr[mid]==target) return mid;
-            if(arr[low]<=arr[mid]){
-                if(arr[low]<=target && target<=arr[mid])
-                   high = mid-1;
-                else low = mid+1;
-            } else {
-                if(arr[mid]<=target && target<=arr[high])
-                   low = mid+1;
-                else high = mid-1;
-            }
-        }
-        return -1;
-    }
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        return binarysearch(nums, n, target);
+          int low = 0, high = nums.size()-1;
+  while(low<=high){
+    int mid = low + (high-low)/2;
+    if(nums[mid]==target) return mid;
+    if(nums[mid]>=nums[low]){
+      if(nums[mid]>=target && nums[low]<=target) high = mid-1;
+      else low = mid+1;
+    }else {
+      if(nums[mid]<=target && target<=nums[high]) low = mid+1;
+      else high = mid-1;
+    }
+  }
+  return -1;
     }
 };
