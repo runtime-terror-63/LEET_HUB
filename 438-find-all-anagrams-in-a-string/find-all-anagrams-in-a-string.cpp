c144a1bf75d9227f933549852f15ded5bb9b135e
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2025-04-08 02:12:22 +0530

    Time: 7 ms (53.57%) | Memory: 11.6 MB (42.36%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        map<char, int>mpp;
-        for(auto it : p) mpp[it]++;
-        vector<int>ans;
-        int k = p.size();
-        int cnt = mpp.size();
-        int windowStart = 0;
-        for(auto windowEnd = 0; windowEnd<s.length(); windowEnd++){
-            if(mpp.find(s[windowEnd]) != mpp.end()){
-                mpp[s[windowEnd]]--;
-                if(mpp[s[windowEnd]]==0) cnt--;
-            }
+  unordered_map<char, int>mpp;
+  vector<int>ds;
+  for(auto it : p) mpp[it]++;
+  int k = p.size();
+  int cnt = mpp.size();
+  int windowStart = 0;
+  for(auto windowEnd = 0; windowEnd<s.length(); windowEnd++){
+    if(mpp.find(s[windowEnd]) != mpp.end()){
+      mpp[s[windowEnd]]--;
+      if(mpp[s[windowEnd]]==0) cnt--;
+    }
 
-            if(windowEnd-windowStart+1==k) {
-                if(cnt==0) ans.push_back(windowStart);
+    if(windowEnd-windowStart+1==k){
+      if(cnt==0) ds.push_back(windowStart);
 
-                //slide the window
-                if(mpp.find(s[windowStart]) != mpp.end()){
-                    mpp[s[windowStart]]++;
-                    if( mpp[s[windowStart]]==1) cnt++;
-                }
-
-                 windowStart++;
-            }
-
-           
-        }
-        return ans;
+      //slide the window
+      if(mpp.find(s[windowStart]) != mpp.end()){
+        mpp[s[windowStart]]++;
+        if(mpp[s[windowStart]]==1) cnt++;
+      }
+      windowStart++;
+    }
+  }
+  return ds;
     }
 };
