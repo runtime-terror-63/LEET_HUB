9a961c2303b045f05344fde642dca81b045c229d
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2025-04-08 10:24:42 +0530

    Time: 2 ms (32.92%) | Memory: 59.3 MB (59.03%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int maxLen = INT_MIN;
-        int zero_cnt = 0;
-        int windowStart = 0;
-        int n = nums.size();
-        for(auto windowEnd = 0; windowEnd<n; windowEnd++){
-            if(nums[windowEnd]==0) zero_cnt++;
-
-            if(zero_cnt>k){
-                if(nums[windowStart]==0) zero_cnt--;
-
-                windowStart++;
-            }
-
-            maxLen = max(maxLen, windowEnd-windowStart+1);
-        }
-        return maxLen;
+         int windowStart = 0;
+  int zeroCnt = 0;
+  int maxWindow = INT_MIN;
+  for(auto windowEnd = 0; windowEnd<nums.size(); windowEnd++){
+    if(nums[windowEnd]==0) zeroCnt++;
+    if(zeroCnt>k){
+      if(nums[windowStart]==0) 
+      zeroCnt--;
+      windowStart++;
+    }
+    maxWindow = max(maxWindow, windowEnd-windowStart+1);
+  }
+  return maxWindow;
     }
 };
