5ea83d865acd57565536f06410900869d2321937
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2025-04-11 23:04:21 +0530

    Time: 34 ms (19.00%) | Memory: 36.2 MB (60.07%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -3,39 +3,40 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+
+        vector<vector<int>>vis(n, vector<int>(m, -1));
+
+        queue<pair<pair<int, int>,int>>q;
         
-        queue<pair<int, pair<int, int>>> q;
-        vector<vector<int>> ans(n, vector<int>(m, -1)); 
-        for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (grid[i][j] == 0) {  
-                q.push({0, {i, j}});
-                ans[i][j] = 0;  
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+            if(grid[i][j]==0){
+                q.push({{i, j}, 0});
+                vis[i][j] = 0;
+            }
             }
         }
-    }
-        
-        int delrow[] = {-1, 1, 0, 0};
-        int delcol[] = {0, 0, 1, -1};
-        
-        //bfs
+
+        int delRow[4] = {-1, 0, +1, 0};
+        int delCol[4] = {0, +1, 0, -1};
+
         while(!q.empty()){
-            int dis = q.front().first;
-            int row = q.front().second.first;
-            int col = q.front().second.second;
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int dist = q.front().second;
             q.pop();
-            
-            for(int i = 0; i<4; i++){
-                int nrow = row+ delrow[i];
-                int ncol = col+ delcol[i];
-                
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && ans[nrow][ncol]==-1){
-                    q.push({dis + 1, {nrow, ncol}});
-                    ans[nrow][ncol] = dis+1;
+
+            for(int i=0; i<4; i++){
+            int nr = row + delRow[i];
+            int nc = col + delCol[i];
+
+            if(nr>=0 && nr<n && nc>=0 && nc<m && 
+                vis[nr][nc]==-1){
+                    vis[nr][nc] = dist+1;
+                    q.push({{nr, nc}, dist+1});
                 }
             }
-            
         }
-        return ans;
+            return vis;
     }
 };
