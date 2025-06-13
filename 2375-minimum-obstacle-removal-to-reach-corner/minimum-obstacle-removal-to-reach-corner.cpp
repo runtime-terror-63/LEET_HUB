7281b5029f84df396c9a781f96d505fbd0fc9c12
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2025-06-13 11:15:51 +0530

    Time: 308 ms (41.17%) | Memory: 157.1 MB (39.60%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    int minimumObstacles(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> distance(m, vector<int>(n, INT_MAX));
-        deque<pair<int, int>> dq;
+    int dx[4] = {-1, +1, 0, 0};
+int dy[4] = {0, 0, -1, +1};
 
-        distance[0][0] = 0;
-        dq.push_front({0, 0});
-        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+int minimumObstacles(vector<vector<int>>& grid) {
+  int n = grid.size();
+  int m = grid[0].size();
+  vector<int> dist(n * m, 1e9);
+  queue<pair<int, int>> q;
+  q.push({0, 0});
+  dist[0] = 0;
 
-        while (!dq.empty()) {
-            auto [x, y] = dq.front();
-            dq.pop_front();
-            for (auto [dx, dy] : directions) {
-                int nx = x + dx, ny = y + dy;
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
-                    int newDist = distance[x][y] + grid[nx][ny];
-                    if (newDist < distance[nx][ny]) {
-                        distance[nx][ny] = newDist;
-                        if (grid[nx][ny] == 0) {
-                            dq.push_front({nx, ny});
-                        } else {
-                            dq.push_back({nx, ny});
-                        }
-                    }
-                }
-            }
+  while (!q.empty()) {
+    int r = q.front().first;
+    int c = q.front().second;
+    q.pop();
+    int cost = r * m + c;
+
+    for (int i = 0; i < 4; i++) {
+      int nr = r + dx[i];
+      int nc = c + dy[i];
+      if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+        int nextNode = nr * m + nc;
+        int newCost = dist[cost] + grid[nr][nc];
+        if (newCost < dist[nextNode]) {
+          dist[nextNode] = newCost;
+            q.push({nr, nc});
         }
-        return distance[m-1][n-1];
+      }
     }
+  }
+
+  return dist[n * m - 1];
+}
 };
