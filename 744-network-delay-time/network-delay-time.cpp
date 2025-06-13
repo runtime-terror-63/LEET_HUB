fade087a3d758c0f527cbb224aa934bc591e2edc
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2025-06-13 13:15:13 +0530

    Time: 77 ms (83.77%) | Memory: 44.4 MB (60.96%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> adj(n + 1); 
-        for (int i = 0; i < times.size(); i++) {
-            int u = times[i][0], v = times[i][1], w = times[i][2];
-            adj[u].push_back({v, w});
+        vector<vector<pair<int, int>>> adj(n + 1);
+        for (auto& edge : times) {
+            int u = edge[0], v = edge[1], wt = edge[2];
+            adj[u].push_back({v, wt});
         }
 
-        vector<int> vis(n + 1, 1e9); 
-        vis[k] = 0; 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, k}); // {distance, node}
+        vector<int> dist(n + 1, 1e9);
+        dist[k] = 0;
+
+  
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        pq.push({0, k});
 
         while (!pq.empty()) {
-            int wei = pq.top().first; 
-            int node = pq.top().second; 
+            auto [d, node] = pq.top();
             pq.pop();
 
-            for (auto it : adj[node]) {
-                int adjNode = it.first;
-                int wt = it.second;
-
-                if (vis[adjNode] > wei + wt) {
-                    vis[adjNode] = wei + wt;
-                    pq.push({vis[adjNode], adjNode});
+            for (auto& [nei, wt] : adj[node]) {
+                if (d + wt < dist[nei]) {
+                    dist[nei] = d + wt;
+                    pq.push({dist[nei], nei});
                 }
             }
         }
 
-        int ans = 0;
+        int maxTime = 0;
         for (int i = 1; i <= n; i++) {
-            if (vis[i] == 1e9) return -1; 
-            ans = max(ans, vis[i]);
+            if (dist[i] == 1e9) return -1;
+            maxTime = max(maxTime, dist[i]);
         }
-        return ans;
+
+        return maxTime;
     }
 };
