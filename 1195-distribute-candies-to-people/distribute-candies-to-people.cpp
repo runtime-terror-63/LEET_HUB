1df4767243a86a7258a3c6a6a3068204548ca07c
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2025-06-14 12:58:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (50.92%) - LeetSync

---
+++ 1195-distribute-candies-to-people/distribute-candies-to-people.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+// Input: candies = 7, num_people = 4
+// Output: [1,2,3,1]
+
+// Input: candies = 10, num_people = 3
+// Output: [5,2,3]
+vector<int> distributeCandies(int candies, int num_people) {
+  vector<int>arr(num_people, 0);
+  int i = 0, give = 1;
+  for(;;){
+    if(candies==0) break;
+    if(i==num_people) i = 0;
+        if(candies>=give){
+          arr[i] += give;
+          candies -= give;
+        }else{
+          arr[i] += candies;
+          candies = 0;
+        }
+     give++;
+     i++;
+  }
+  return arr;
+    }
+};
