c3aaf6a675c0c0c7e641283f9fc1887a696a0a75
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2025-06-18 13:23:02 +0530

    Time: 31 ms (19.06%) | Memory: 70.5 MB (46.96%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,49 +1,31 @@
 class Solution {
 public:
-    bool func(vector<int> &arr, int n, int m, int mid, int k)
-{
-  int l = 0, cnt = 0;
-  for (int i = 0; i < n; i++)
-  {
-    if (arr[i] <= mid)
-    {
-      l++;
-      if (l == k){
-        cnt++;
-        l=0;
-      }
+bool fn(vector<int>&arr, int mid, int m, int k){
+  int n  =arr.size();
+  int cnt = 0, noCnt = 0;
+  for(int i=0; i<n; i++){
+    if(arr[i]<=mid){
+      cnt++;
+    }else {
+      noCnt += (cnt/k);
+      cnt = 0;
     }
-    else
-      l = 0;
   }
 
-  if (cnt >= m)
-    return true;
-  return false;
+  noCnt+=(cnt/k);
+  return noCnt>=m;
 }
-int bs(vector<int> &arr, int n, int m, int k)
-{
-  int low = *min_element(arr.begin(), arr.end());
-  int high = *max_element(arr.begin(), arr.end());
-  int ans = 0;
-  while (low <= high)
-  {
-    int mid = (low + high) >> 1;
-    if (func(arr, n, m, mid, k))
-    {
+int minDays(vector<int>& bloomDay, int m, int k) {
+  int low = *min_element(bloomDay.begin(), bloomDay.end());
+  int high = *max_element(bloomDay.begin(), bloomDay.end());
+  int ans = -1;
+  while(low<=high){
+    int mid = (low+high)/2;
+    if(fn(bloomDay, mid, m, k)){
       ans = mid;
-      high = mid - 1;
-    }
-    else
-      low = mid + 1;
-  }
- // cout<<ans<<endl;
+      high = mid-1;
+    }else low = mid+1;
+  } 
   return ans;
-}
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        int n = bloomDay.size();
-        int ans = bs(bloomDay, n, m, k); 
-        if(ans==0) return -1;
-        else return ans;
     }
 };
