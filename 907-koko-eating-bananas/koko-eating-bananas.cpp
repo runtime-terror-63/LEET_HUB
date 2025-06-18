6a8f519482f0c2e8f39112f955e8eb1f419843a1
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2025-06-18 11:30:44 +0530

    Time: 18 ms (14.91%) | Memory: 22.8 MB (73.80%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
-    long long func(vector<int> &arr, int n, int mid, int h)
-{
-  long long total = 0;
-  for (int i = 0; i < n; i++)
-  {
-    total += ceil((double)(arr[i]) / (double)(mid));
+    long long  fn(vector<int>&piles, int mid, int h){
+  long long  cnt = 0;
+  for(auto it : piles){
+    cnt += ceil((double)it/(double)mid);
   }
-  return total;
+  return cnt;
 }
-int findAns(vector<int> &arr, int n, int h)
-{
-  int low = 1;
-  int high = *max_element(arr.begin(), arr.end());
-  int ans = INT_MAX;
-  while (low <= high)
-  {
-    int mid = (low + high) >> 1;
-    long long  ele = func(arr, n, mid, h);
-    if (ele <= h)
-    {
-      ans = min(ans, mid);
-      high = mid - 1;
-    }
-    else
-      low = mid + 1;
+
+int minEatingSpeed(vector<int>& piles, int h) {
+  int low = 1, high = *max_element(piles.begin(), piles.end());
+  int ans = -1;
+  while(low<=high){
+    int mid = (low+high)/2;
+    if(fn(piles, mid, h)<=h){
+      ans = mid;
+      high = mid-1;
+    }else low = mid+1;
   }
   return ans;
-}
-int minEatingSpeed(vector<int> &piles, int h)
-{
-  int n = piles.size();
-  return findAns(piles, n, h);
 }
 };
