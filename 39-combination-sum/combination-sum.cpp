dcd8b1b90e7686db3166a6cea46c1c62aaed013c
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2025-06-24 20:56:32 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (54.52%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-   void solve(int ind, vector<vector<int>>&ans, vector<int>&ds, vector<int>&candidates, int n, int target){
-    if(ind==n){
-        if(target==0) ans.push_back(ds);
-        return ;
+void fn(vector<int>&arr, int ind,  int target, vector<vector<int>>&ans, vector<int>&ds){
+      if(ind==arr.size()){
+    if(target==0){
+      ans.push_back(ds);
     }
+    return ;
+  }
+  //pick
+  if(arr[ind]<=target){
+    ds.push_back(arr[ind]);
+    fn(arr, ind, target-arr[ind], ans, ds);
+    ds.pop_back();
+  }
 
-    //pick
-    if(candidates[ind]<=target){
-        ds.push_back(candidates[ind]);
-        solve(ind, ans, ds, candidates, n,  target-candidates[ind]);
-        ds.pop_back();
-    }
-    //not-pick
-    solve(ind+1, ans, ds, candidates, n,  target);
-   }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        int n = candidates.size();
+  //not-pick
+  fn(arr, ind+1, target, ans, ds);
+}
+vector<vector<int>>combinationSum(vector<int>&arr, int target){
         vector<vector<int>>ans;
         vector<int>ds;
-        solve(0, ans, ds,candidates, n, target);
+        fn(arr, 0, target, ans, ds);
         return ans;
     }
 };
