1cfccedeb99b28be2116a06ee181c2dd6faca931
Sayan Dutta <94905513+runtime-terror-63@users.noreply.github.com>
2025-06-25 21:23:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (71.40%) - LeetSync

--- 857-positions-of-large-groups/positions-of-large-groups.cpp
+++ 857-positions-of-large-groups/positions-of-large-groups.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> largeGroupPositions(string s) {
-          int n = s.length();
-  int i=0, j=0;
-  vector<vector<int>>ds;
-  while(i<n){
-    while(j<n && s[i]==s[j])j++;
+        int n = s.length();
+        int i=0, j=0;
+        vector<vector<int>>ds;
+        while(i<n){
+            while(j<n && s[i]==s[j])j++;
 
-    if(j-i>=3) ds.push_back({i, j-1});
-    i = j;
-  }
-  return ds;
+            if(j-i>=3) ds.push_back({i, j-1});
+            i = j;
+        }
+        return ds;
     }
 };
