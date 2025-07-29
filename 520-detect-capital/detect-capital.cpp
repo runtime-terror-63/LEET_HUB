47f3216e9c4179184f3e540249508dbfd8316ef2
Sayan Dutta <sayandutta84314@gmail.com>
2025-07-29 23:07:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (18.04%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     bool upper(string word){
-        for(char c : word){
-            if(!isupper(c)) return false;
+        for(auto it : word){
+            if(!isupper(it)) return false;
         }
-        return true;
+      return true;
     }
-
     bool lower(string word){
-        for(char c : word){
-            if(!islower(c)) return false;
+        for(auto it : word){
+            if(!islower(it)) return false;
         }
         return true;
     }
-
     bool detectCapitalUse(string word) {
-        if (upper(word) || lower(word) || (isupper(word[0]) && lower(word.substr(1))))
-            return true;
-        else
-            return false;
+        if(upper(word) || lower(word) || (isupper(word[0]) && lower(word.substr(1)))) return true;
+        else return false;
     }
 };
